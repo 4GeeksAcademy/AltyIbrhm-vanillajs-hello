10887a70670f033e7f708a3678363ed555cc85fa
AltyIbrhm <87036697+AltyIbrhm@users.noreply.github.com>
2024-09-30 22:57:54 +0000

    RandomCardGenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let cardNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "J", "Q", "A"];
+  let cardType = ["♥", "♦", "♣", "♠"];
+
+  let newCard = `${getRandomArrayIndex(cardNumber)}`;
+  let newCardType = `${getRandomArrayIndex(cardType)}`;
+
+  console.log(newCardType);
+  document.querySelector(".cardNumber").innerHTML = newCard;
+  document.querySelector(".cardTypeUpper").innerHTML = newCardType;
+  document.querySelector(".cardTypeBottom").innerHTML = newCardType;
 };
+
+function getRandomArrayIndex(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
